93cf2a79f9ba496369d4c54c5534ae269f77bbc5
damondan <damon-r-d-1976@protonmail.com>
2026-02-10 15:26:41 -0600

    added type for videoUrl

---
+++ src/routes/taurihub/+page.server.js
@@ -0,0 +1,5 @@
+export function load() {
+    return {
+        videoUrl: "https://servetaurihubvid-production.up.railway.app/videos"
+    };
+}
--- src/routes/taurihub/+page.svelte
+++ src/routes/taurihub/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-
-
+	/** @type {{ videoUrl: string }} */
+	export let data;
 </script>
 
 <div class="taurihubVidContainer flex flex-col items-center">
     <video 
-        src="/BlurTauriHubForPort2.mp4" controls 
+        src={data.videoUrl} controls 
         class="w-full max-w-4xl rounded-lg shadow-lg"
-    />
+    >
+		<track kind="captions" />
+	</video>
     <div class="docsearch-arch mt-4 sm:mt-6 lg:mt-8 w-full">
 		<h1 class="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-medium text-gray-800 text-center mb-6">
 			Architecture
