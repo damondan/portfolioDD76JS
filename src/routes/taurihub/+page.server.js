179e65e1ecb17debec5784d28d2f87b5d0a508c4
damondan <damon-r-d-1976@protonmail.com>
2026-02-10 15:37:09 -0600

    committing

--- src/routes/taurihub/+page.server.js
+++ src/routes/taurihub/+page.server.js
@@ -1,5 +1,5 @@
 export function load() {
     return {
-        videoUrl: "https://servetaurihubvid-production.up.railway.app/videos"
+        videoUrl: "https://servetaurihubvid-production.up.railway.app/videos/BlurTauriHubForPort2.mp4"
     };
 }
